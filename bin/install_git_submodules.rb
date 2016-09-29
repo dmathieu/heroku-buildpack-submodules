f9c38b517eb5231173779dfdce091c2f63c4c69e
John Ryan <john.p.ryan4@gmail.com>
2016-09-29 15:39:04 -0700

    use master if branch is not specified

--- bin/install_git_submodules.rb
+++ bin/install_git_submodules.rb
@@ -5,9 +5,15 @@ config = ParseConfig.new("#{ENV['BUILD_DIR']}/.gitmodules")
 config.get_params.each do |param|
   next unless param.match(/^submodule/)
   c = config[param]
+  
+  branch = c["branch"]
+  
+  if branch == nil
+    branch = "master"
+  end
 
   puts "---> Installing submodule #{c["path"]}"
-  `git clone #{c["url"]} -b #{c["branch"]} #{ENV['BUILD_DIR']}/#{c["path"]}`
+  `git clone #{c["url"]} -b #{branch} #{ENV['BUILD_DIR']}/#{c["path"]}`
 
   puts "----> Removing submodule git folder"
   `rm -rf #{ENV['BUILD_DIR']}/#{c["path"]}/.git`
