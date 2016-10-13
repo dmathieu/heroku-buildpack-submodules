78b02863f156f364fd65333a44464c36c098f877
John Ryan <john.p.ryan4@gmail.com>
2016-10-13 14:46:15 -0700

    use branch_flag for brevity

--- bin/install_git_submodules.rb
+++ bin/install_git_submodules.rb
@@ -5,15 +5,10 @@ config = ParseConfig.new("#{ENV['BUILD_DIR']}/.gitmodules")
 config.get_params.each do |param|
   next unless param.match(/^submodule/)
   c = config[param]
-  
-  branch = c["branch"]
 
   puts "---> Installing submodule #{c["path"]}"
-  if branch
-    `git clone #{c["url"]} -b #{branch} #{ENV['BUILD_DIR']}/#{c["path"]}`
-  else
-    `git clone #{c["url"]} #{ENV['BUILD_DIR']}/#{c["path"]}`
-  end
+  branch_flag = c["branch"] ? "-b #{c['branch']}" : ""
+  `git clone #{c["url"]} #{branch_flag} #{ENV['BUILD_DIR']}/#{c["path"]}`
   
   puts "----> Removing submodule git folder"
   `rm -rf #{ENV['BUILD_DIR']}/#{c["path"]}/.git`
