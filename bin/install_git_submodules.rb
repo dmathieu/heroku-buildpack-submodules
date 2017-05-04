35622728df8757a1bacde155290fcd2960ff1a11
dmathieu <42@dmathieu.com>
2017-05-04 09:27:49 +0200

    install gems within the buildpack directory

--- bin/install_git_submodules.rb
+++ bin/install_git_submodules.rb
@@ -1,3 +1,6 @@
+require 'rubygems'
+Gem.clear_paths
+
 require 'parseconfig'
 
 config = ParseConfig.new("#{ENV['BUILD_DIR']}/.gitmodules")
@@ -9,7 +12,7 @@ config.get_params.each do |param|
   puts "---> Installing submodule #{c["path"]}"
   branch_flag = c["branch"] ? "-b #{c['branch']}" : ""
   `git clone #{c["url"]} #{branch_flag} #{ENV['BUILD_DIR']}/#{c["path"]}`
-  
+
   puts "----> Removing submodule git folder"
   `rm -rf #{ENV['BUILD_DIR']}/#{c["path"]}/.git`
 end
