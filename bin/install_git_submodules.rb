0f4d21ce172a0739c06d29f4d006c6c82ad61518
Chris McCormick <chris@mccormick.cx>
2019-01-05 10:11:42 +0800

    Tidy messages as per Heroku buildpack message spec.

--- bin/install_git_submodules.rb
+++ bin/install_git_submodules.rb
@@ -9,10 +9,10 @@ config.get_params.each do |param|
   next unless param.match(/^submodule/)
   c = config[param]
 
-  puts "---> Installing submodule #{c["path"]}"
+  puts "----> Installing submodule #{c["path"]} #{c["branch"]}"
   branch_flag = c["branch"] ? "-b #{c['branch']}" : ""
   `git clone #{c["url"]} #{branch_flag} #{ENV['BUILD_DIR']}/#{c["path"]}`
 
-  puts "----> Removing submodule git folder"
+  puts "      Removing submodule git folder"
   `rm -rf #{ENV['BUILD_DIR']}/#{c["path"]}/.git`
 end
