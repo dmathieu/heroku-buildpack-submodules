cc443ca92dd5395814604f8beaf939ad2f14e575
Chris McCormick <chris@mccormick.cx>
2019-01-05 10:54:25 +0800

    Fix up messages to comply with Heroku style.

--- bin/install_git_submodules.rb
+++ bin/install_git_submodules.rb
@@ -9,16 +9,16 @@ config.get_params.each do |param|
   next unless param.match(/^submodule/)
   c = config[param]
 
-  puts "----> Installing submodule #{c["path"]} #{c["branch"]}"
+  puts "-----> Installing submodule #{c["path"]} #{c["branch"]}"
   branch_flag = c["branch"] ? "-b #{c['branch']}" : ""
   `git clone --single-branch #{c["url"]} #{branch_flag} #{ENV['BUILD_DIR']}/#{c["path"]}`
   if c.key?("revision")
-    puts "      Setting submodule revision to #{c["revision"]}"
+    puts "       Setting submodule revision to #{c["revision"]}"
     Dir.chdir(c["path"]) do
       `git reset --hard #{c["revision"]}`
     end
   end
 
-  puts "      Removing submodule git folder"
+  puts "       Removing submodule git folder"
   `rm -rf #{ENV['BUILD_DIR']}/#{c["path"]}/.git`
 end
